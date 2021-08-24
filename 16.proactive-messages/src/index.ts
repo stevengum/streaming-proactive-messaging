a3fc038838506361ad88fc475453460333e01d43
stevengum <14935595+stevengum@users.noreply.github.com>
2021-08-23 17:14:42 -0700

    update index.ts, add index.html

--- 16.proactive-messages/src/index.ts
+++ 16.proactive-messages/src/index.ts
@@ -1,47 +1,30 @@
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License.
-
 import { config } from 'dotenv';
 import * as path from 'path';
 import * as restify from 'restify';
 
-// Import required bot services.
-// See https://aka.ms/bot-services to learn more about the different parts of a bot.
-import { BotFrameworkAdapter } from 'botbuilder';
-
-// This bot's main dialog.
+import { CloudAdapter, ConfigurationBotFrameworkAuthentication } from 'botbuilder';
 import { EchoBot } from './bot';
 
 const ENV_FILE = path.join(__dirname, '..', '.env');
 config({ path: ENV_FILE });
 
-// Create HTTP server.
 const server = restify.createServer();
-server.listen(process.env.port || process.env.PORT || 3978, () => {
-    console.log(`\n${server.name} listening to ${server.url}`);
-    console.log('\nGet Bot Framework Emulator: https://aka.ms/botframework-emulator');
-    console.log('\nTo talk to your bot, open the emulator select "Open Bot"');
+const port = process.env.port || process.env.PORT || 3978;
+server.listen(port, () => {
+    console.log(`\n${server.name} listening to http://localhost:3978`);
+    console.log(`\nTo talk to this bot, navigate to http://localhost:3978`);
 });
 
 server.use(restify.plugins.bodyParser({
     mapParams: true
 }));
 
-// Create adapter.
-// See https://aka.ms/about-bot-adapter to learn more about adapters.
-const adapter = new BotFrameworkAdapter({
-    appId: process.env.MicrosoftAppId,
-    appPassword: process.env.MicrosoftAppPassword
-});
+const auth = new ConfigurationBotFrameworkAuthentication(process.env as any)
+const adapter = new CloudAdapter(auth);
 
-// Catch-all for errors.
 const onTurnErrorHandler = async (context, error) => {
-    // This check writes out errors to console log .vs. app insights.
-    // NOTE: In production environment, you should consider logging this to Azure
-    //       application insights.
     console.error(`\n [onTurnError] unhandled error: ${ error }`);
 
-    // Send a trace activity, which will be displayed in Bot Framework Emulator
     await context.sendTraceActivity(
         'OnTurnError Trace',
         `${ error }`,
@@ -49,44 +32,32 @@ const onTurnErrorHandler = async (context, error) => {
         'TurnError'
     );
 
-    // Send a message to the user
     await context.sendActivity('The bot encountered an error or bug.');
     await context.sendActivity('To continue to run this bot, please fix the bot source code.');
 };
 
-// Set the onTurnError for the singleton BotFrameworkAdapter.
 adapter.onTurnError = onTurnErrorHandler;
 
-// Create the main dialog.
 const conversationReferences = {};
 const myBot = new EchoBot(conversationReferences);
 
-// Listen for incoming requests.
 server.post('/api/messages', (req, res) => {
-    adapter.processActivity(req, res, async (context) => {
-        // Route to main dialog.
+    adapter.process(req, res, async (context) => {
         await myBot.run(context);
     });
 });
-
-// Listen for Upgrade requests for Streaming.
 server.on('upgrade', (req, socket, head) => {
-    // Create an adapter scoped to this WebSocket connection to allow storing session data.
-    const streamingAdapter = new BotFrameworkAdapter({
-        appId: process.env.MicrosoftAppId,
-        appPassword: process.env.MicrosoftAppPassword
-    });
-    // Set onTurnError for the BotFrameworkAdapter created for each connection.
-    streamingAdapter.onTurnError = onTurnErrorHandler;
-
-    streamingAdapter.useWebSocket(req, socket, head, async (context) => {
-        // After connecting via WebSocket, run this logic for every request sent over
-        // the WebSocket connection.
-        await myBot.run(context);
-    });
+    adapter.process(req, socket as any, head, async (context) => await myBot.run(context));
+});
+server.opts('/api/messages', (req, res) => {
+    res.send(200);
 });
 
-// Listen for incoming notifications and send proactive messages to users.
+server.get('/', restify.plugins.serveStatic({
+    directory: path.join(__dirname, '../public'),
+    file: 'index.html'
+}));
+
 server.get('/api/notify', async (req, res) => {
     for (const conversationReference of Object.values(conversationReferences)) {
         await adapter.continueConversation(conversationReference, async turnContext => {
@@ -99,7 +70,6 @@ server.get('/api/notify', async (req, res) => {
     res.end();
 });
 
-// Listen for incoming custom notifications and send proactive messages to users.
 server.post('/api/notify', async (req, res) => {
     for (var prop in req.body) {
         var msg = req.body[prop];
